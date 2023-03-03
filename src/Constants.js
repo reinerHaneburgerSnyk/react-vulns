02054a21870ddb0ff80cfd7c3567fb3a3921457b
Kinnaird McQuade <kmcquade@users.noreply.github.com>
2023-03-03 11:30:45 -1000

    Update Constants.js
    Using the localhost deployment of Javulna (the backend) instead of the google cloud instance mentioned in the README.

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,11 @@
 import  {Component} from 'react';
 
 class Constants extends Component {
-  static backendUrlBase = "http://35.204.241.43:31337/";
-  static backendRESTUrlBase = "http://35.204.241.43:31337/rest/";
+  static backendUrlBase = "http://localhost:8080/";
+  static backendRESTUrlBase = "http://localhost:8080/rest/";
+  // The below URLs are the google cloud instance mentioned in the README. Changing this to the localhost deployment of Javulna
+  // static backendUrlBase = "http://35.204.241.43:31337/";
+  // static backendRESTUrlBase = "http://35.204.241.43:31337/rest/";
 }
 
 export default Constants;
