fbbb1da7b815b85f5a6ae2148a8045b48fdc3610
Kinnaird McQuade <kmcquade@users.noreply.github.com>
2023-03-29 12:07:53 -1000

    Update Constants.js
    If BACKEND_URL_BASE environment variable is set, use it as the value of backendUrlBase; if BACKEND_REST_URL_BASE is set, use it as the value of backendRESTUrlBase; otherwise, use local defaults.

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,8 @@
-import  {Component} from 'react';
+import { Component } from 'react';
 
 class Constants extends Component {
-  static backendUrlBase = "http://localhost:8080/";
-  static backendRESTUrlBase = "http://localhost:8080/rest/";
+  static backendUrlBase = process.env.BACKEND_URL_BASE || "http://localhost:8080/";
+  static backendRESTUrlBase = process.env.BACKEND_REST_URL_BASE || "http://localhost:8080/rest/";
   // The below URLs are the google cloud instance mentioned in the README. Changing this to the localhost deployment of Javulna
   // static backendUrlBase = "http://35.204.241.43:31337/";
   // static backendRESTUrlBase = "http://35.204.241.43:31337/rest/";
